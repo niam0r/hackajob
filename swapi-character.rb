2dd717fe6185bcff5d60afce189159ba0ef6a54c
Romain niam0r <niamor@mm.st>
2019-04-01 19:14:00 +0100

    solved

--- swapi-character.rb
+++ swapi-character.rb
@@ -1,11 +1,13 @@
 require 'net/http'
+require 'json'
 
 class Solution
-    def run(character)
-      # Some work here; return type and arguments should be according to the problem's requirements
-      uri = URI("https://challenges.hackajob.co/swapi/api/people/?search=#{character}")
-        Net::HTTP.get(uri) # => String
-        numberOfFilms = nil
-    return numberOfFilms
-    end
+  def run(character)
+    uri = URI("https://challenges.hackajob.co/swapi/api/people/?search=#{character}")
+    res = Net::HTTP.get(uri)
+    result = JSON.parse(res)
+    result['results'][0]['films'].length
+  end
 end
+
+puts Solution.new.run('Yoda')
