2636d2ce0e6ca9e3f20ff77c8432c627bc1bb901
Romain niam0r <niamor@mm.st>
2019-04-01 20:39:38 +0100

    working for characters_from_film

--- swapi_characters_film.rb
+++ swapi_characters_film.rb
@@ -8,12 +8,21 @@ class Solution
 
     uri = URI("https://challenges.hackajob.co/swapi/api/films/?search=#{film}")
     result = JSON.parse(Net::HTTP.get(uri))
-    characters_links = result['results'][0]['characters']
-    puts characters_links
+    characters_urls = result['results'][0]['characters']
+    characters_urls.each do |url|
+      res = JSON.parse(Net::HTTP.get(URI(url)))
+      characters_from_film << res['name']
+      character_filmography_urls = res['films']
+    end
+
+
 
     character_filmography << 'none' if character_filmography.empty?
     "#{film}: #{characters_from_film.join(', ')}; #{character}: #{character_filmography.join(', ')}"
   end
 end
 
-Solution.new.run("The Force Awakens", "Walter White")
+puts Solution.new.run("The Force Awakens", "Walter White")
+# Solution.new.run("The Force Awakens", "Walter White")
+
+# puts Solution.new.get_name('https://challenges.hackajob.co/swapi/api/people/1/')
