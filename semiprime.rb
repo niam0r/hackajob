fb67a211271a9267cf928e2878eaf546b00a5dc1
Romain niam0r <niamor@mm.st>
2019-04-02 18:55:43 +0100

    solved, need to find more efficient solution

--- semiprime.rb
+++ semiprime.rb
@@ -1,8 +1,18 @@
 require 'prime'
 
 class Solution
-    def run(number)
-
-
+  def run(number)
+    possible_prime_factors = []
+    Prime.each(number) { |prime| possible_prime_factors << prime }
+    products_array = []
+    possible_prime_factors.to_a.each do |factor_a|
+      possible_prime_factors.to_a.each do |factor_b|
+        products_array << factor_a * factor_b
+      end
     end
+    !products_array.find_index(number).nil?
+  end
 end
+
+puts Solution.new.run(91)
+# Solution.new.run(91)
